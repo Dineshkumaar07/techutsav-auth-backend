0816500407322c35e566207762932ec6728dce44
venkadesh004 <venkadeshdeveloper@gmail.com>
2024-03-04 13:25:33 +0530

    fix get events

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -7,6 +7,6 @@ router.get("/getFirst", eventControllers.entryEventData_get);
 router.get("/getAll", eventControllers.alleventData_get);
 router.post("/getSingleEvent", eventControllers.singleEvent_post);
 router.get("/getFlagshipEvents", eventControllers.getFlagShipEvents_get);
-router.post('/getSpecificEvents', profileControllers.getEvents_post);
+router.post('/getSpecificEvents', eventControllers.getEvents_post);
 
 module.exports = router;
